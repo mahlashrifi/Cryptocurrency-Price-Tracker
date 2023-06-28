2eb124dfa494a09860f348dbec6940acc6c1a791
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2023-06-28 22:15:01 +0330

    add search crypto form in home page and allow cors on backend

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,22 @@
-import './style.css'
+import './style.css';
+
+document.querySelector('#search-form').addEventListener('submit', event => {
+  event.preventDefault()
+  const input = document.querySelector('#search-form input')
+  const query = input.value
+  input.value = ''
+  fetch(`http://127.0.0.1:8000/peyk/price?q=${query}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      const ul = document.querySelector('#results')
+      ul.innerHTML = ''
+      data.forEach(item => {
+        const li = document.createElement('li')
+        li.textContent = item
+        ul.appendChild(li)
+      })
+    }).catch(err => {
+      console.log(err)
+    })
+});
--- services/main.py
+++ services/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from peyk import router as peyk_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
 
 app.include_router(peyk_router, prefix="/peyk")
