26060097cea8062a5fba5773a843903c02c8cd63
mahlashrifi <mahla7997@gmail.com>
2023-06-28 22:34:47 +0330

    Complete get-price_history

--- services/peyk.py
+++ services/peyk.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request, Depends
 from sqlalchemy.orm import Session
 from database import SessionLocal, Alert, Price
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import desc
+
 
 router = APIRouter()
-
 
 def get_db():
     db = SessionLocal()
@@ -16,7 +18,12 @@ def get_db():
 
 @router.get("/price")
 async def get_price_history(data: Request, db: Session = Depends(get_db)):
-    return ""
+    
+    data = await data.json()
+    coin_name = data["coin_name"]
+    prices = db.query(Price).filter(Price.coin_name == coin_name).order_by(desc(Price.time)).all()
+    prices_json = jsonable_encoder(prices)
+    return {"prices": prices_json}
 
 
 @router.post("/subscribe")
@@ -38,4 +45,5 @@ async def subscribe_alert(data: Request, db: Session = Depends(get_db)):
 async def get_alerts(data: Request, db: Session = Depends(get_db)): 
     alerts = db.query(Alert).all()
     return {"alerts": alerts}
+
 
